8fc8c18caaba405ffe7e8d70f0bba36a62738743
petehilljnr <petehilljnr@gmail.com>
2025-09-10 19:08:42 +1200

    update env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTOvkii2wiHLAP9e7o35M-8ZG-LhvM8Ts",
-  authDomain: "stantec-dpg.firebaseapp.com",
-  projectId: "stantec-dpg",
-  storageBucket: "stantec-dpg.appspot.com",
-  messagingSenderId: "239190953187",
-  appId: "1:239190953187:web:cc0ca323cd8974fcf389d6",
-  measurementId: "G-1L2E04LJB5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
