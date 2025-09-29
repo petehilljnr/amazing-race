94bbfca0bb92f964976d1e2c749b2e4f6ebfc517
petehilljnr <petehilljnr@gmail.com>
2025-09-29 23:37:22 +1300

    Add rollup-plugin-visualizer for build statistics and optimize chunking in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({
+      filename: 'stats.html', // output file
+      open: true,             // auto-open in browser
+      gzipSize: true,         // show gzip sizes
+      brotliSize: true        // show brotli sizes
+    })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom', 'zustand'],
+          'chakra-vendor': ['@chakra-ui/react', '@chakra-ui/icons', '@emotion/react', '@emotion/styled', 'framer-motion'],
+          'firebase-vendor': ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          'leaflet-vendor': ['leaflet', 'react-leaflet'],
+          'dexie-vendor': ['dexie', 'dexie-react-hooks'],
+          'heic-vendor': ['heic2any']        
+        },
+      },
+    },
+  },
 })
